c552f881f876e4db6bdb4848d98dbc864eb749f5
Michael Rivnak <mrivnak86@gmail.com>
2021-01-02 02:12:55 -0500

    added some graphics libraries and register struct

---
+++ src/cpu.rs
@@ -0,0 +1,17 @@
+struct Registers {
+	a: u8,
+	b: u8,
+	c: u8,
+	d: u8,
+	e: u8,
+	f: u8,
+	h: u8,
+	l: u8,
+}
+
+impl Registers {
+	// TODO: add functions for all the registers
+	fn get_bc() {
+
+	}
+}
