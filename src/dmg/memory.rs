b744acd9f4549860aa056568bf18256c14fbea8e
Michael Rivnak <rivnakm1@gmail.com>
2022-09-07 22:33:45 -0400

    basic mem unit tests

--- src/dmg/memory.rs
+++ src/dmg/memory.rs
@@ -1,3 +1,7 @@
+use std::vec;
+
+use super::data::Address;
+
 const MEM_SIZE: usize = 0x10000;
 
 #[derive(Clone, Debug, PartialEq)]
@@ -7,6 +11,50 @@ pub struct MemoryBus {
 
 impl MemoryBus {
     pub fn create() -> MemoryBus {
-        MemoryBus { memory: Vec::with_capacity(MEM_SIZE)}
+        MemoryBus { memory: vec![0; MEM_SIZE]}
+    }
+
+    pub fn write(&mut self, addr: Address, data: u8) {
+        self.memory[addr as usize] = data;
+    }
+
+    pub fn read(&self, addr: Address) -> u8 {
+        self.memory[addr as usize]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_write() {
+        let mut mem = MemoryBus::create();
+
+        let addresses = vec![0x0000, 0x00FF, 0xFFFF];
+        let values = vec![0x0F, 0xF0, 0xAA];
+
+        for addr in &addresses {
+            for val in &values {
+                mem.write(*addr, *val);
+                assert_eq!(*val, mem.memory[*addr as usize])
+            }
+        }
+    }
+
+    #[test]
+    fn test_read() {
+        let mut mem = MemoryBus::create();
+
+        let addresses = vec![0x0000, 0x00FF, 0xFFFF];
+        let values = vec![0x0F, 0xF0, 0xAA];
+
+        for addr in &addresses {
+            for val in &values {
+                mem.memory[*addr as usize] = *val;
+                let result = mem.read(*addr);
+                assert_eq!(result, *val)
+            }
+        }
     }
 }
