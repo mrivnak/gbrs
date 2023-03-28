d6e0494cec7cc8a04a9312560d9f7f511200d405
Mike Rivnak <rivnakm1@gmail.com>
2023-03-28 17:30:53 -0400

    fix release script for new layout

--- res/scripts/SetVersion.ps1
+++ res/scripts/SetVersion.ps1
@@ -1,5 +1,5 @@
 $version = $args[0].replace("v", "")
 
-$file = ".\src\Cargo.toml"
+$file = ".\gbrs\src\Cargo.toml"
 
 ((Get-Content -path $file -Raw) -replace '0.0.0',$version) | Set-Content -Path $file
